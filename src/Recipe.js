f711e5502cb600337d3a9be9eb85e71b1908ad83
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-02-17 20:39:46 -0500

    Make details display details

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import RecipeDetails from "./RecipeDetails"
 import style from './recipe.module.css';
 
 const Recipe = ({title,calories,image,ingredients,healthLabels,totalNutrients,url}) =>{
+	const [show, setShow] = useState(false)
 	return(
 		<div className={style.recipe}>
 			
 			<h1>{title}</h1>
 			<img className={style.image} src={image} alt=""/>
-			<button className={style.recipeDetails}>Details</button>
-			<RecipeDetails ingredients={ingredients}/>			
+			<button onClick={()=> setShow(!show)} className={style.recipeDetails}>Details</button>
+			{show && <RecipeDetails ingredients={ingredients}/>}			
 		</div>
 	);
 
