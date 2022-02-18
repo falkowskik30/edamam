afdfd15a01fca03ee78cb2253ad9ec03bbb39ca9
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-02-18 09:50:36 -0500

    Add healthlabels to recipe details

--- src/RecipeDetails.js
+++ src/RecipeDetails.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import style from './recipe.module.css'
-import {v4 as uuidv4} from "uuid"
+import React from 'react';
+import style from './recipe.module.css';
+import {v4 as uuidv4} from "uuid";
 
 const RecipeDetails = ({ingredients, healthLabels}) => {
-    return  ingredients.map(ingredient => {
-        return (
-            <ul key={uuidv4} className={style.ingredientList}>
+    return (
+        <><il key={uuidv4} className={style.ingredientList}>
+            {ingredients.map(ingredient => (
                 <li className={style.ingredientText}>{ingredient.text}</li>
-            </ul>
-        )   
-    })
-}
+            ))}
+        </il><il key ={uuidv4} className={style.healthLabelsList}>
+                {healthLabels.map(healthLabels=>(
+                    <li>{healthLabels}</li>
+                ))}
+            </il></>
+    )
+};
 
-export default RecipeDetails
+export default RecipeDetails;
