61dd15eb3facd660f099607d85785737a688f0cd
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-02-21 14:53:35 -0500

    Add radio buttons for rating system

--- src/RecipeDetails.js
+++ src/RecipeDetails.js
@@ -16,6 +16,23 @@ const RecipeDetails = ({ingredients, healthLabels, calories, Nutrients, url}) =>
                 ))}
             </il>
         <p className={style.calorieText}>Total Calories: {calories}</p>
+        <div class={style.rating}>
+            <input type="radio" id="star5" name="rating" value="5">
+            </input>
+            <label for="star5"></label>
+            <input type="radio" id="star4" name="rating" value="4">
+            </input>
+            <label for="star4"></label>
+            <input type="radio" id="star3" name="rating" value="3">
+            </input>
+            <label for="star3"></label>
+            <input type="radio" id="star2" name="rating" value="2">
+            </input>
+            <label for="star2"></label>
+            <input type="radio" id="star1" name="rating" value="1">
+            </input>
+            <label for="star1"></label>
+        </div>
         <a href={url} target="_blank" rel="noopener noreferrer">Full Recipe</a>
             </>
     )
