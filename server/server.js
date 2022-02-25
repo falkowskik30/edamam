40608d32cafbd1cb2bad8e319fbd4bcae0b71391
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-02-25 10:57:53 -0500

    Test backend
    Setup backend

--- server/server.js
+++ server/server.js
@@ -1,3 +1,18 @@
 const express = require('express')
 const axios = require('axios')
-const 
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+
+app.get('/recipes/:query', async (req,res) => {
+    const response = await axios.get(`https://api.edamam.com/search?q=${req.params.query}&app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}`)
+    res.json(response.data.hits)
+    //console.log(response.data.hits)
+})
+
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}`))
