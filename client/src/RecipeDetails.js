ec4c454a62d8e3a03f938e7b048363a649a5a648
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-07 12:13:00 -0500

    Confirm accordion test successful

--- client/src/RecipeDetails.js
+++ client/src/RecipeDetails.js
@@ -1,31 +1,42 @@
-import React from 'react';
+//import React from 'react';
 import style from './recipe.module.css';
 import {v4 as uuidv4} from "uuid";
 //import StarRatingComponent from 'react-star-rating-component';
 import {Rating} from 'react-simple-star-rating';
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const RecipeDetails = ({ingredients, healthLabels, calories, Nutrients, url}) => {
+    console.log(ingredients);
     return (
-        <><il key={uuidv4} className={style.ingredientList}>
-            {ingredients.map(ingredient => (
-                <li className={style.ingredientText}>{ingredient.text}</li>
-            ))}
-        </il>
-        <h2>Health Labels</h2>
-        <il key ={uuidv4} className={style.healthLabelsList}>
-                {healthLabels.map(healthLabels=>(
-                    <li>{healthLabels}</li>
-                ))}
-            </il>
-        <p className={style.calorieText}>Total Calories: {calories}</p>
-        <div className={style.StarBox}>
-            <Rating></Rating>
-        </div>
-        <div className={style.RecipeButton}>
-        <a href={url} target="_blank" rel="noopener noreferrer">Full Recipe</a>
-        </div>
+        <>
+        <Accordion>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panella-content"
+                id="panella-header"
+                >
+                    <Typography>Ingredients</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        <il key={uuidv4} className={style.ingredientList}>
+                            {ingredients.map(ingredient => (
+                                <li className={style.ingredientText}>{ingredient.text}</li>
+                            ))}
+                        </il>
+                    </Typography>
+                </AccordionDetails>
+        </Accordion>
+
+        <Rating></Rating>
             </>
     )
 };
 
 export default RecipeDetails;
+
