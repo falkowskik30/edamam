4a313edae6590ca1242332e0808872c91cca697f
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-16 14:30:36 -0400

    Correct file path

--- client/src/pages/recipes.js
+++ client/src/pages/recipes.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {v4 as uuidv4} from 'uuid'
-import './App.css';
-import Recipe from './Recipe';
+import '../App.css';
+import Recipe from '../Recipe';
 import axios from 'axios';
 
 const App = () => {
